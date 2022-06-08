9750103152a2833edf5451d6dbb8b8e3995ee179
WeeHorse <benjaminberglund@gmail.com>
2022-06-08 10:20:47 +0200

    cleaning the text

--- structure.js
+++ structure.js
@@ -8,6 +8,21 @@ log(typeof text)
 
 text = "" + text // gör om objekt till sträng
 
-let sentences = text.match(/[^\.!\?]+[\.!\?]+/g)
+text = text.split("PART ONE")[2] // hämta ut relevant text (tar bort bokens index)
 
-log(sentences.length)
+text = text.split("\n\n").join(". ") // splitta dubbla radbrytningar till meningar
+
+text = text.split("\n").join(" ") // splitta enkla radbrytningar til mellanslag
+
+let sentences = text.match(/[^\.!\?]+[\.!\?]+/g) // splitta texten till meningar
+
+let index = []
+
+for(let sentence of sentences){
+    let s = {
+        sentence: sentence.trim() // ta bort whitespace runt meningen
+    }
+    index.push(s)
+}
+
+log(index[103])
